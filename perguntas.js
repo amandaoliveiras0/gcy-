713bd6f0ee98f91709fabf508d2325af59e5bb90
amandaoliveiras0 <00001102060112SP@al.educacao.sp.gov.br>
2024-11-14 10:10:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual e o maior bioma brasileiro?',
+    'A Amazonia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'Moscou'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a maior espécie de animais?',
+    'A baleia-azul '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz respeito em inglês?',
+    'Respeito em ingles é respect'
+)
